8867623c3160db08c3879bb4d4474a21e610d07c
Daniel Yeh <dyeh1989@gmail.com>
2022-01-17 21:19:11 -0800

    git status

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,18 @@
+var person = {
+  firstName: 'Daniel',
+  lastName: 'Yeh',
+  hobby: 'Tennis'
+};
+
+console.log(person);
+
+var fullName = person.firstName + ' ' + person.lastName;
+console.log(fullName);
+
+person.job = 'Coder';
+console.log(person.job);
+
+person.previousJob = 'Scientist';
+console.log(person.previousJob);
+
+console.log(person);
