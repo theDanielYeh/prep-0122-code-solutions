6e404802d6d691c346c40f0dd340fdb22bf80419
Daniel Yeh <dyeh1989@gmail.com>
2022-01-20 20:58:29 -0800

    Completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,19 @@
+// create your loops here.
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+let x = 0;
+for (let i = 0; i < 10; i++) {
+  console.log(x);
+  x += 2;
+
+}
+
+// for (var z = 0; z < 10; z++) {
+//   console.log(z * 2);
+// }
+
+for (let i = 100; i > 0; i--) {
+  console.log('Time till explosion: ' + i);
+}
